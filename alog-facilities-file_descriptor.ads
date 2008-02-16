66e66715c0647254c49f7e3b814d3d113a58498f
Reto Buerki <buerki@swiss-it.ch>
2008-02-16 16:28:57 +0100

    Fixed typo in license text.

--- alog-facilities-file_descriptor.ads
+++ alog-facilities-file_descriptor.ads
@@ -15,7 +15,7 @@
 --  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 --  GNU Lesser General Public License for more details.
 --
---  You should have received a copy of t4he GNU Lesser General Public License
+--  You should have received a copy of the GNU Lesser General Public License
 --  along with Alog; if not, write to the Free Software
 --  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 --  MA  02110-1301  USA
